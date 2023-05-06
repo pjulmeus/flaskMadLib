9fdce47adb86116e425cac954920ce2dadcfc0e9
pjulmeus <pjulmeus@gmail.com>
2023-05-05 20:37:02 -0400

    final commit

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template , request
 from flask_debugtoolbar import DebugToolbarExtension
 from stories import story
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "oh-so-secret"
+app.debug = True
 
 debug = DebugToolbarExtension(app)
 
-@app.route("/")
+@app.route('/')
 def homepage():
-    place = request.args["place"]
-    return render_template('madlib.html', place=place)
+    prompt = story.prompts 
+    return render_template("madlib.html", prop = prompt)
+
+@app.route('/mad')
+def stories(): 
+    text = story.generate(request.args)
+    return render_template("madlib_stories.html", text = text)
